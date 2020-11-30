2f11e5233356b877c1d46ee401d062d92c6d1360
Justin Barca <justinbarca@gmail.com>
2020-11-30 10:04:19 -0500

    no cairo, no toys, add build.sh, fiddle with CMakeLists.txt

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+emcmake cmake . \
+    -DDoubleConversion_INCLUDE_DIR:PATH=${LIBS_DIR}/double-conversion/ \
+    -DDoubleConversion_LIBRARY:FILEPATH=${LIBS_DIR}/double-conversion/libdouble-conversion.a \
+    -DGLIB_INCLUDE_DIR:PATH=${LIBS_DIR}/glib-emscripten/target/include \
+    -DGLIB_LIBRARY:FILEPATH=${LIBS_DIR}/glib-emscripten/target/lib/libglib-2.0.a \
+    -DGSL_INCLUDE_DIR:PATH=${LIBS_DIR}/gsl \
+    -DGSL_LIBRARY:FILEPATH=${LIBS_DIR}/gsl/libgsl.a \
+    -DGSL_CBLAS_LIBRARY:FILEPATH=${LIBS_DIR}/gsl/libgslcblas.a \
+    -D2GEOM_TESTING=OFF \
+    -DCMAKE_CXX_FLAGS="-sUSE_BOOST_HEADERS=1" \
+    -DCMAKE_C_FLAGS="-sUSE_BOOST_HEADERS=1" \
+&& make
