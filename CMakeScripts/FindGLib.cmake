f7c97ad79891ea28a548e0c05f4c1980e955ce5e
Justin Barca <justinbarca@gmail.com>
2020-11-30 16:47:02 -0500

    author FindGLib.cmake and favor single cache arg, opinionated find

--- CMakeScripts/FindGLib.cmake
+++ CMakeScripts/FindGLib.cmake
@@ -1,83 +1,24 @@
-# FindGLIB.cmake
-# <https://github.com/nemequ/gnome-cmake>
-#
-# CMake support for GLIB/GObject/GIO.
-#
-# License:
-#
-#   Copyright (c) 2016 Evan Nemerson <evan@nemerson.com>
-#
-#   Permission is hereby granted, free of charge, to any person
-#   obtaining a copy of this software and associated documentation
-#   files (the "Software"), to deal in the Software without
-#   restriction, including without limitation the rights to use, copy,
-#   modify, merge, publish, distribute, sublicense, and/or sell copies
-#   of the Software, and to permit persons to whom the Software is
-#   furnished to do so, subject to the following conditions:
-#
-#   The above copyright notice and this permission notice shall be
-#   included in all copies or substantial portions of the Software.
-#
-#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-#   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-#   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-#   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-#   DEALINGS IN THE SOFTWARE.
-
-find_package(PkgConfig)
-
-if(PKG_CONFIG_FOUND)
-  pkg_search_module(GLIB_PKG glib-2.0)
-endif()
-
-find_library(GLIB_LIBRARY glib-2.0 HINTS ${GLIB_PKG_LIBRARY_DIRS})
-set(GLIB glib-2.0)
-
-if(GLIB_LIBRARY AND NOT GLIB_FOUND)
-  add_library(${GLIB} SHARED IMPORTED)
-  set_property(TARGET ${GLIB} PROPERTY IMPORTED_LOCATION "${GLIB_LIBRARY}")
-  set_property(TARGET ${GLIB} PROPERTY INTERFACE_COMPILE_OPTIONS "${GLIB_PKG_CFLAGS_OTHER}")
-
-  find_path(GLIB_INCLUDE_DIRS "glib.h"
-    HINTS ${GLIB_PKG_INCLUDE_DIRS}
-    PATH_SUFFIXES "glib-2.0")
-
-  get_filename_component(GLIB_LIBDIR "${GLIB}" DIRECTORY)
-  find_path(GLIB_CONFIG_INCLUDE_DIR "glibconfig.h"
-    HINTS
-      ${GLIB_LIBDIR}
-      ${GLIB_PKG_INCLUDE_DIRS}
-    PATHS
-      "${CMAKE_LIBRARY_PATH}"
-    PATH_SUFFIXES
-      "glib-2.0/include"
-      "glib-2.0")
-  unset(GLIB_LIBDIR)
-
-  if(GLIB_CONFIG_INCLUDE_DIR)
-    file(STRINGS "${GLIB_CONFIG_INCLUDE_DIR}/glibconfig.h" GLIB_MAJOR_VERSION REGEX "^#define GLIB_MAJOR_VERSION +([0-9]+)")
-    string(REGEX REPLACE "^#define GLIB_MAJOR_VERSION ([0-9]+)$" "\\1" GLIB_MAJOR_VERSION "${GLIB_MAJOR_VERSION}")
-    file(STRINGS "${GLIB_CONFIG_INCLUDE_DIR}/glibconfig.h" GLIB_MINOR_VERSION REGEX "^#define GLIB_MINOR_VERSION +([0-9]+)")
-    string(REGEX REPLACE "^#define GLIB_MINOR_VERSION ([0-9]+)$" "\\1" GLIB_MINOR_VERSION "${GLIB_MINOR_VERSION}")
-    file(STRINGS "${GLIB_CONFIG_INCLUDE_DIR}/glibconfig.h" GLIB_MICRO_VERSION REGEX "^#define GLIB_MICRO_VERSION +([0-9]+)")
-    string(REGEX REPLACE "^#define GLIB_MICRO_VERSION ([0-9]+)$" "\\1" GLIB_MICRO_VERSION "${GLIB_MICRO_VERSION}")
-    set(GLIB_VERSION "${GLIB_MAJOR_VERSION}.${GLIB_MINOR_VERSION}.${GLIB_MICRO_VERSION}")
-    unset(GLIB_MAJOR_VERSION)
-    unset(GLIB_MINOR_VERSION)
-    unset(GLIB_MICRO_VERSION)
-
-    list(APPEND GLIB_INCLUDE_DIRS ${GLIB_CONFIG_INCLUDE_DIR})
-    set_property(TARGET ${GLIB} PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${GLIB_INCLUDE_DIRS}")
-  endif()
-endif()
-
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GLIB
-    REQUIRED_VARS
-      GLIB_LIBRARY
-      GLIB_INCLUDE_DIRS
-    VERSION_VAR
-      GLIB_VERSION)
+
+find_path(GLIB_LIBRARY PATHS ${GLIB_DIR})
+find_path(GLIB_INCLUDE_DIR glib-2.0/libglib-2.0.a PATHS ${GLIB_DIR})
+
+find_path(GLIB_CONFIG_INCLUDE_DIR glib-2.0/include/glibconfig.h
+    PATHS ${GLIB_DIR})
+
+find_package_handle_standard_args(GLIB DEFAULT_MSG
+  GLIB_DIR
+  GLIB_LIBRARY
+  GLIB_INCLUDE_DIR
+  GLIB_CONFIG_INCLUDE_DIR)
+
+mark_as_advanced(
+    GLIB_DIR
+    GLIB_LIBRARY
+    GLIB_INCLUDE_DIR
+    GLIB_CONFIG_INCLUDE_DIR)
+
+if(GLIB_FOUND)
+  set(GLIB_LIBRARIES ${GLIB_LIBRARY})
+  set(GLIB_INCLUDE_DIRS ${GLIB_INCLUDE_DIR} ${GLIB_INCLUDE_})
+endif()
--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 emcmake cmake . \
     -DDoubleConversion_INCLUDE_DIR:PATH=${LIBS_DIR}/double-conversion/ \
     -DDoubleConversion_LIBRARY:FILEPATH=${LIBS_DIR}/double-conversion/libdouble-conversion.a \
-    -DGLIB_PKG_INCLUDE_DIRS:PATH=${LIBS_DIR}/glib-emscripten/target/ \
-    -DGLIB_PKG_LIBRARY_DIRS:FILEPATH=${LIBS_DIR}/glib-emscripten/target/lib/libglib-2.0.a \
+    -DGLIB_DIR:PATH=${LIBS_DIR}/glib-emscripten/target/ \
     -DGSL_INCLUDE_DIR:PATH=${LIBS_DIR}/gsl \
     -DGSL_LIBRARY:FILEPATH=${LIBS_DIR}/gsl/libgsl.a \
     -DGSL_CBLAS_LIBRARY:FILEPATH=${LIBS_DIR}/gsl/libgslcblas.a \
